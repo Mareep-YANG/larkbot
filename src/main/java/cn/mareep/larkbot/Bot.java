94e7fa9b4d9e044279c898bc11dfb802cb56cdff
Mareep <mareep@mareep.cn>
2025-05-18 15:16:34 +0800

    feat: enhance Bot configuration management and add logging support

--- src/main/java/cn/mareep/larkbot/Bot.java
+++ src/main/java/cn/mareep/larkbot/Bot.java
@@ -4,13 +4,29 @@ package cn.mareep.larkbot;
 import cn.mareep.larkbot.listener.GroupMessageListener;
 import cn.mareep.larkbot.listener.PrivateMessageListener;
 import com.lark.oapi.ws.Client;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Bot {
     public static Client client;
-    public static ConfigurationManager config = new ConfigurationManager();
+    private static final ConfigurationManager config = new ConfigurationManager();
+    private static final Logger logger = LoggerFactory.getLogger(Bot.class);
     public static void main(String[] args) {
         new GroupMessageListener();
         new PrivateMessageListener();
+        initConfig();
+        client = new Client.Builder(config.get("larkAppId"),config.get("larkAppSecret"))
+                .eventHandler(LarkEventDispatcher.getDispatcher())
+                .build();
+        client.start();
+    }
+    public static ConfigurationManager getConfig() {
+        return config;
+    }
+    public static Logger getLogger() {
+        return logger;
+    }
+    private static void initConfig(){
         config.load();
         if (!config.containsKey("wordpressUsername")){
             config.set("wordpressUsername", "username");
@@ -21,13 +37,30 @@ public class Bot {
         if (!config.containsKey("wordpressUrl")){
             config.set("wordpressUrl", "https://example.com");
         }
+        if (!config.containsKey("larkAppId")){
+            config.set("larkAppId", "larkAppId");
+        }
+        if (!config.containsKey("larkAppSecret")){
+            config.set("larkAppSecret", "larkAppSecret");
+        }
+        if (!config.containsKey("tencentCloudSecretId")){
+            config.set("tencentCloudSecretId", "tencentCloudSecretId");
+        }
+        if (!config.containsKey("tencentCloudSecretKey")){
+            config.set("tencentCloudSecretKey", "tencentCloudSecretKey");
+        }
+        if (!config.containsKey("tencentCloudRegion")){
+            config.set("tencentCloudRegion", "tencentCloudRegion");
+        }
+        if (!config.containsKey("tencentCloudBucketName")){
+            config.set("tencentCloudBucketName", "tencentCloudBucketName");
+        }
+        if (!config.containsKey("tencentCloudImagePath")){
+            config.set("tencentCloudImagePath", "tencentCloudImagePath");
+        }
+        if (!config.containsKey(" ")){
+            config.set("imageBaseUrl", "imageBaseUrl");
+        }
         config.save();
-        client = new Client.Builder("cli_a88e1ef34ab9d00d","oyOtibq8lxb9jFUHmqAqchUzfEtTxqla")
-                .eventHandler(LarkEventDispatcher.getDispatcher())
-                .build();
-        client.start();
-    }
-    public static ConfigurationManager getConfig() {
-        return config;
     }
 }
